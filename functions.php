2d0fa9294a66246efde211fd9151c9d14f314fc1
joshuaiz <joshuaiz@me.com>
2017-12-30 15:44:40 -0600

    Fixed ptags function

--- functions.php
+++ functions.php
@@ -433,10 +433,10 @@ function disable_emojicons_tinymce( $plugins ) {
 
 
 // Remove the p from around imgs (http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/)
-function filter_ptags_on_images($content){
+function plate_filter_ptags_on_images($content){
     return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
-add_filter('the_content', 'filter_ptags_on_images');
+add_filter('the_content', 'plate_filter_ptags_on_images');
 
 
 // This removes the annoying […] to a Read More link
